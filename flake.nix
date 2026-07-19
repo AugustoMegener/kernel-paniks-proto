071680398a92189928d8d1d5bd98aef9ea6d6937
AugustoMegener <76854495+AugustoMegener@users.noreply.github.com>
2026-07-19 19:44:28 -0300

    [Publish] 0.0.1

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-
   outputs = { self, nixpkgs }: let
     meta = builtins.fromTOML (builtins.readFile "${self}/meta.toml");
     system = "x86_64-linux";
@@ -8,10 +7,9 @@
   in {
     packages.${system}.default = pkgs.runCommand "build-project" { } ''
       mkdir -p $out/${meta.version}
-      chmod -R u+w proto/
-      tar -cf $out/${meta.version}/kernel-paniks-proto-${meta.version}.tar \
-        -C ${self} proto/
+      cp -r ${self}/proto ./proto
+      chmod -R u+w ./proto
+      tar -cf $out/${meta.version}/kernel-paniks-proto-${meta.version}.tar proto/
     '';
   };
-
 }
